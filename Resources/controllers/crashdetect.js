c6fd59202e63f283fe194151e5ae9057125a97b9
Alex Wong <alexwong@bu.edu>
2015-02-08 14:23:27 -0500

    Slightly modify alert behavior for CrashDetect

--- Resources/controllers/crashdetect.js
+++ Resources/controllers/crashdetect.js
@@ -29,37 +29,66 @@ function updateAccelData (e) {
         var data = e.acceleration;
         if (Math.abs(lastX - data.x) > CRASH_THRESHOLD || Math.abs(lastY - data.y) > CRASH_THRESHOLD || Math.abs(lastY - data.y) > CRASH_THRESHOLD) {
             accelX++;
-            alert("Crash detected!");
-            var emailDialog = Ti.UI.createEmailDialog();
-            emailDialog.subject = "Crash Detected for Alex Wong";
-            emailDialog.toRecipients = ['contact@lyfecycle.me'];
-            emailDialog.messageBody = '<b>Alex Wong may have been in an accident at location</b>' + coordinates;
-            emailDialog.open();
-            ///// Upload point to api
-            var xhr = Ti.Network.createHTTPClient();
 
-            xhr.onload = function(e) {
-                console.log('onload');
-                console.log('response: ' + this.responseText);
-                //handle response, which at minimum will be an HTTP status code
-            };
-            xhr.open('POST','http://lyfecycle-api.herokuapp.com/locations');
-            xhr.setRequestHeader('Content-Type','application/json');
-            xhr.send({
-                name:'Alex Crashed',
-                longitude: coordinates[0],
-                latitude: coordinates[1]
-            });
+            // Run alert process
+            crashAlert();            
         }
+
         lastX = data.x;
         lastY = data.y;
         lastZ = data.z;
 
+        // Attitude = measurement of orientation of a device
         data = e.attitude;
         
     } else {
         if (e.error) Ti.API.error(e.error);
     }
+}
+
+function crashAlert() {
+    
+    // Display an alert dialog that is cancellable
+    var dialog = Ti.UI.createAlertDialog({
+        cancel: 1,
+        buttonNames: ['Confirm', 'Cancel'],
+        message: 'Cancel crash alert',
+        title: 'Crash Alert'
+    });
+    dialog.addEventListener('click', function(e){
+        if (e.index === e.source.cancel){
+          Ti.API.info('The cancel button was clicked');
+    }
+        Ti.API.info('e.cancel: ' + e.cancel);
+        Ti.API.info('e.source.cancel: ' + e.source.cancel);
+        Ti.API.info('e.index: ' + e.index);
+    });
+    dialog.show();
+
+    // Notify emergency contacts and personnel
+    // Currently it is only an email...
+    var emailDialog = Ti.UI.createEmailDialog();
+    emailDialog.subject = "Crash Detected for Rider";
+    emailDialog.toRecipients = ['contact@lyfecycle.me'];
+    //emailDialog.messageBody = '<b>Alex Wong may have been in an accident at location</b>' + coordinates;
+    emailDialog.messageBody = '<b>Rider may have been in an accident at location</b>';
+    emailDialog.open();
+
+    // Add crash information to database
+    var xhr = Ti.Network.createHTTPClient();
+
+    xhr.onload = function(e) {
+        console.log('onload');
+        console.log('response: ' + this.responseText);
+        // handle response, which at minimum will be an HTTP status code
+    };
+    xhr.open('POST','http://lyfecycle-api.herokuapp.com/locations');
+    xhr.setRequestHeader('Content-Type','application/json');
+    xhr.send({
+        name:'Rider has crashed',
+        //longitude: coordinates[0],
+        //latitude: coordinates[1]
+    });
 }
 
 module.exports = CrashDetect;
