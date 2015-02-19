b41c186732149eda48c8b01fc2de11834985360d
Alex Wong <alexwong@bu.edu>
2015-02-19 16:14:08 -0500

    Cleanup app.js

--- Resources/app.js
+++ Resources/app.js
@@ -10,38 +10,12 @@ var userCrashDetect = new CrashDetect();
 var windowController = new WindowController();
 
 
-windowController.goToMain();
-
-
-/*
 // ***** Windows *****
 
 // Main Window -------------------------------
 // The main Window is opened upon opening the app.
 // the Window controller controls all other window switching
-var main = Titanium.UI.createWindow();
-
-var mainWindowMapView = mainMap.getMapView();
-
-// main.add(mainWindowMapView);
-main.add(homeView.main);
-main.open();
-
-
-// Login Window ------------------------------
-var login = Titanium.UI.createWindow();
-
-
-// If not logged in, open Login window
-if (userSession.getLoginState == false) {
-	login.open();
-}
-
-// Settings Window ---------------------------
-var settings = Titanium.UI.createWindow();
-
-*/
-
+windowController.goToMain();
 
 
 
