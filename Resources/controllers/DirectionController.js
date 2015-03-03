c871747622c3f0fe6cbacf5b2931ad69238ba06c
Kevin Mannix <kmannix@bu.edu>
2015-03-03 11:03:58 -0500

    Added comments of DirectionController.js to make move to backend easier

--- Resources/controllers/DirectionController.js
+++ Resources/controllers/DirectionController.js
@@ -14,36 +14,41 @@ DirectionController.prototype.setUserPin = function(){
 
 DirectionController.prototype.getDirections = function(endLocation) {
 	var self = this;
+	// SHOULD BE MOVED TO THE BACKEND
+		// To send to the backend, will send current location (long & lat) along with desination (will be an address string)
+		// From the API we should receive the directions, and any incidents that might occur along each step of the route
 	var requestURL =  Constants.GoogleDirectionsStartReq + gpsLocationController.getCurrentLatitude() + ',' 
 					+ gpsLocationController.getCurrentLongitude() + '&destination=' + endLocation + Constants.GoogleDirectionsEndReq;	  
 
+	// Request & callbacks
 	var client = Ti.Network.createHTTPClient({
 		onload: function(e) { addDirectionsToComponents(JSON.parse(this.responseText), endLocation); },
 		onerror: function(e) { alert("Could not get directions. Try again soon!"); }, timeout : 10000
 	 });
 	client.open("GET", encodeURI(requestURL)); client.send();
 
-	function addDirectionsToComponents(json, endLocation) {
-		try {
-			var jsonSteps = json['routes'][0]['legs'][0]['steps'], steps = [], polyline = [];
-			for (i in jsonSteps) {
-				directionView.mapComponent.decodePolyline(jsonSteps[i]['polyline']['points'], polyline);
-				steps.push(StepModel.validStep(jsonSteps[i]));
-			}
-			addDirectionsToMapComponent({'steps':steps, 'end_destination_text': endLocation, 'polyline_route': polyline});
-			addDirectionsToRouteListComponent({'steps':steps, 'end_destination_text': endLocation, 'polyline_route': polyline});
-		} catch(err) { alert("Could not get location! "); console.log(err); };
-	};
+	// ALL HELPER FUNCTIONS
+		function addDirectionsToComponents(json, endLocation) {
+			try {
+				var jsonSteps = json['routes'][0]['legs'][0]['steps'], steps = [], polyline = [];
+				for (i in jsonSteps) {
+					directionView.mapComponent.decodePolyline(jsonSteps[i]['polyline']['points'], polyline);
+					steps.push(StepModel.validStep(jsonSteps[i]));
+				}
+				addDirectionsToMapComponent({'steps':steps, 'end_destination_text': endLocation, 'polyline_route': polyline});
+				addDirectionsToRouteListComponent({'steps':steps, 'end_destination_text': endLocation, 'polyline_route': polyline});
+			} catch(err) { alert("Could not get location! "); console.log(err); };
+		};
 
-	function addDirectionsToMapComponent(stepsObject){
-		self.mapComponentController.setPolyline(stepsObject);
-		self.mapComponentController.setNewDelta(stepsObject['steps']);
-		self.mapComponentController.setRouteAnnotations(stepsObject);
-	};
+		function addDirectionsToMapComponent(stepsObject){
+			self.mapComponentController.setPolyline(stepsObject);
+			self.mapComponentController.setNewDelta(stepsObject['steps']);
+			self.mapComponentController.setRouteAnnotations(stepsObject);
+		};
 
-	function addDirectionsToRouteListComponent(stepsObject){
-		self.routeListComponentController.showList(stepsObject);
-	};
+		function addDirectionsToRouteListComponent(stepsObject){
+			self.routeListComponentController.showList(stepsObject);
+		};
 };
 
 DirectionController.prototype.showCurrentLocation = function(){
